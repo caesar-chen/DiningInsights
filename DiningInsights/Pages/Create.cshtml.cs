9428459c5137577099a68f1f870232560f7b86ca
Yixun Zhang <yixzhang@microsoft.com>
2018-07-25 18:18:41 -0700

    clean up

--- DiningInsights/Pages/Create.cshtml.cs
+++ DiningInsights/Pages/Create.cshtml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using DiningInsights;
 
 namespace DiningInsights.Pages
 {
--- DiningInsights/Pages/Display.cshtml.cs
+++ DiningInsights/Pages/Display.cshtml.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using DiningInsights;
 
 namespace DiningInsights.Pages
 {
